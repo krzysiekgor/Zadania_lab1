727e93bdcbb562955770b798de946f7f2c3b4d1b
Krzysiek <krzysiekgor94@gmail.com>
2022-02-28 16:39:48 +0100

    Praca domowa

---
+++ main.py
@@ -0,0 +1,91 @@
+
+import math
+
+#zadanie1
+imie ="Krzysztof"
+nazwisko = "Górecki"
+
+a=1
+b=2
+
+c=3.2
+d=4.3
+
+e=1+3j
+f=2+4j
+
+# print(imie)
+# print(nazwisko)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#zadanie2
+i=6
+j=3
+# print(i+j)
+# print(i-j)
+# print(i*j)
+# print(i**j)
+# print(i/j)
+# print(i//j)
+# print(i%j)
+
+#zadanie3
+# x=10
+# print(x)
+# x+=1
+# print(x)
+# x-=1
+# print(x)
+# x*=2
+# print(x)
+# x/=2
+# print(x)
+# x**=2
+# print(x)
+# x%=2
+# print(x)
+
+#zadanie4
+
+# print(math.e**10)
+# print(math.pow(math.log(5+math.pow(math.sin(8),2),math.e),6))
+# print(math.floor(3.55))
+# print(math.ceil(4.80))
+
+#zadanie5
+
+imieWielkie = "KRZYSZTOF"
+nazwiskoWielkie ="GÓRECKI"
+
+print(imieWielkie.capitalize()+nazwiskoWielkie.capitalize())
+
+#zadanie6
+
+# slowaPiosenki = "la la la"
+# print(slowaPiosenki.count("la"))
+
+#zadanie7
+
+# ciag = "To jest długi ciąg znaków"
+# print(ciag[0] + ciag[-1])
+# print(ciag[0] + ciag[len(ciag)-1])
+
+#zadanie8
+
+# nastepnaPieknaPiosenka = "Ho Ho Ho"
+# slowa = nastepnaPieknaPiosenka.split(' ')
+# print (slowa)
+
+#zadanie9
+
+znaki = "ciag znaków"
+liczba = 0.0
+kod = 0xDE
+print(znaki)
+print(liczba)
+print(hex(kod))
